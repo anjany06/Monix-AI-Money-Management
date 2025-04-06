0945d4b511f0f9a2ae89345a59ccbee027657373
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-06 22:40:49 +0530

    Add transaction form and transaction schema

--- app/(main)/transaction/_components/transaction-form.jsx
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -1,8 +1,36 @@
 "use client";
+import { createTransaction } from "@/actions/transactions";
+import { transactionSchema } from "@/app/lib/schema";
+import useFetch from "@/hooks/use-fetch";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
 
-const AddTransactionForm = () => {
-  return <div>AddTransactionForm</div>;
+const AddTransactionForm = ({ accounts, categories }) => {
+  useForm({
+    resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      type: "EXPENSE",
+      amount: "",
+      description: "",
+      accountId: accounts.find((ac) => ac.isDefault)?.id,
+      date: new Date(),
+      isRecurring: false,
+    },
+  });
+
+  const {
+    loading: transactionLoading,
+    fn: transactionFn,
+    data: transactionResult,
+  } = useFetch(createTransaction);
+  return <form>
+    {/* AI Receipt Scannner */}
+    <div>
+      <label>Type</label>
+      
+    </div>
+  </form>;
 };
 
 export default AddTransactionForm;
