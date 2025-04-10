379a494f7a26ac31537ea5a1b3fee2c7a05e0dfd
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-11 02:58:46 +0530

    Add new functions and update existing functions in inngest route and functions.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,9 @@
 import { inngest } from "@/lib/inngest/client";
-import { CheckBudgetAlert } from "@/lib/inngest/functions";
+import {
+  CheckBudgetAlert,
+  processRecurringTransaction,
+  triggerRecurrinTransactions,
+} from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
@@ -8,5 +12,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
     CheckBudgetAlert,
+    triggerRecurrinTransactions,
+    processRecurringTransaction,
   ],
 });
