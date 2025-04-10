beb192d4171798c2a0e5ec648368da6472df83f6
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-10 22:46:07 +0530

    Refactor layout and receipt scanner components

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -7,7 +7,9 @@ const AddTransaction = async () => {
   const accounts = await getUserAccounts();
   return (
     <div className="max-w-3xl mx-auto px-5">
-      <h1 className="text-5xl gradient-title">Add Transaction</h1>
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title">Add Transaction</h1>
+      </div>
       <AddTransactionForm accounts={accounts} categories={defaultCategories} />
     </div>
   );
